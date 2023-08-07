da30eaf7f4f6c34fd42b0e33830b0f7a4569a6ae
Jeandre <jeandrejr2003@gmail.com>
2023-08-07 18:05:36 +0200

    New Task6

---
+++ PollApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("poll/<int:poll_id>/", views.PollView.as_view(), name="poll"),
+]
